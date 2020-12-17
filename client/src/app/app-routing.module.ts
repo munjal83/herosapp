708866df2a5ae7524bf91cb0c21ace432be243a9
munjal83 <munjalbpatel@hotmail.com>
2020-12-17 13:52:00 -0500

    Add Error Handling

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 import { HeroListComponent } from './heroes/hero-list/hero-list.component';
 import { HomeComponent } from './home/home.component';
@@ -20,7 +23,10 @@ const routes: Routes = [
       { path: 'messages', component: MessagesComponent },
     ]
   },
-  { path: '**', component: HomeComponent, pathMatch: 'full' },
+  { path: 'errors', component: TestErrorsComponent},
+  { path: 'not-found', component: NotFoundComponent},
+  { path: 'server-error', component: ServerErrorComponent},
+  { path: '**', component: NotFoundComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
